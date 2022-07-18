8174840e58f9130772032e3bca30dafb01b97d58
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-18 14:47:57 -0700

    assignment 4

---
+++ CallingMethods4_TA/CallingMethods4_TA/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods4_TA
+{
+    class Class1
+    {
+        public void subtract2(int int1, int int2)
+        {
+            int int3 = int1 - 2;
+            Console.WriteLine("displaying the second int: {0}", int2);
+        }
+
+    }
+}
---
+++ CallingMethods4_TA/CallingMethods4_TA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods4_TA
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Class1 class1 = new Class1();
+            class1.subtract2(int1: 100, int2: 34);
+
+            endProgram();
+        }
+
+        public static void endProgram()
+        {
+            Console.WriteLine("================= PROGRAM OVER - Enter 'r' to Restart =================");
+            if (Console.ReadLine() == "r")
+            {
+                Main();
+            }
+        }
+    }
+}
