7b395b9f1fd7a83cb1e7467a257ddd6b40c4b9df
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-18 14:38:09 -0700

    optional arguments

---
+++ CallingMethods3_TA/CallingMethods3_TA/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods3_TA
+{
+    class Class1
+    {
+        public int multiplyBy25(int int1, int Int2 = 1)
+        {
+            return int1 * Int2 * 25;
+        }
+
+    }
+}
---
+++ CallingMethods3_TA/CallingMethods3_TA/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods3_TA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 myClass1 = new Class1();
+
+            Console.WriteLine("Enter a number to Multiply by 25:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Enter another number to also multiply, if you'd like, or hit ENTER to skip:");
+            string input2 = Console.ReadLine();
+
+            try
+            {
+
+                int input1Int;
+                int input2Int;
+
+                input1Int = Convert.ToInt32(input1);
+                if (input2 != "")
+                {
+                    input2Int = Convert.ToInt32(input2);
+                    Console.WriteLine(myClass1.multiplyBy25(input1Int, input2Int));
+                }
+                else
+                {
+                    Console.WriteLine(myClass1.multiplyBy25(input1Int));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("input1 = {0}, input2 = {1}", input1,input2);
+                Console.WriteLine("There was an issue: {0}", ex);
+            }
+            finally
+            {
+
+            }
+
+            endProgram();
+
+
+
+        }
+
+        public static void endProgram()
+        {
+            Console.WriteLine("================= PROGRAM OVER - Enter 'r' to Restart =================");
+            if (Console.ReadLine() == "r") {
+                Main();
+            }
+        }
+
+    }
+}
