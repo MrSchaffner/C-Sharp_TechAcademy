7f0e6a7617da497530f54d157586145ddb708c4b
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-18 11:06:47 -0700

    nested folder being committed

---
+++ CallingMethods_TA/CallingMethods_TA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods_TA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number to do three equations on");
+            string input = Console.ReadLine();
+            int toInt = Convert.ToInt32(input);
+            Console.WriteLine("============== Okay, here we go =============");
+            Console.WriteLine(myClass.divideByThree(toInt));
+            Console.WriteLine(myClass.multiplyByZero(toInt));
+            Console.WriteLine(myClass.squareAndSubtractSelf(toInt));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethods_TA/CallingMethods_TA/myClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods_TA
+{
+    class myClass
+    {
+
+        public static int multiplyByZero(int myInt)
+        {
+            return myInt * 0;
+        }
+
+        public static int squareAndSubtractSelf(int myInt)
+        {
+            return myInt * myInt - myInt;
+        }
+
+        public static int divideByThree(int myInt)
+        {
+            return myInt / 3;
+        }
+
+    }
+}
