7d16fbee4ca822d82b24b2dc147c92292d2bbd4c
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-18 15:20:09 -0700

    out parameters

---
+++ CallingMethods5_TA/CallingMethods5_TA/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods5_TA
+{
+    class Class1
+    {
+
+        public void Halve(int inputInt, out int myIntOUT)
+        {
+            myIntOUT = inputInt/2;
+
+
+        }
+
+    }
+}
---
+++ CallingMethods5_TA/CallingMethods5_TA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods5_TA
+{
+    class Program
+    {
+        static void Main()
+        {
+            int IntOUT; 
+            Class1 class1 = new Class1();
+
+            Console.WriteLine("enter a number to halve");
+            string input = Console.ReadLine();
+            int inputInt = Convert.ToInt32(input);
+
+            class1.Halve(inputInt: inputInt, out IntOUT); // input should come from out property
+
+            Console.WriteLine("You don't know the half of it: {0}", IntOUT);
+
+            endProgram();
+        }
+
+        public static void endProgram()
+        {
+            Console.WriteLine("================= PROGRAM OVER - Enter 'r' to Restart =================");
+            if (Console.ReadLine() == "r")
+            {
+                Main();
+            }
+        }
+    }
+}
