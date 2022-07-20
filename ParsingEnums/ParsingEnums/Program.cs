5dbe2290d9561f944a95665b3dbc0e4b524a4071
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-19 22:29:41 -0700

    parse enum

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the day");
+            string input = Console.ReadLine().ToUpper();
+
+            try
+            {
+                DaysOfTheWeek inputDay = (DaysOfTheWeek) Enum.Parse( typeof(DaysOfTheWeek), input);
+                Console.WriteLine("You entered a valid enum: {0}", inputDay);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week. Exception: {0}", ex);
+            }
+
+            endProgram();
+        }
+
+
+        public static void endProgram()
+        {
+            Console.WriteLine("================= PROGRAM OVER - Enter 'r' to Restart =================");
+            if (Console.ReadLine() == "r")
+            {
+                Main();
+            }
+        }
+
+        public enum DaysOfTheWeek
+        {
+            SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+        }
+    }
+}
