277190cefb526867497f503482dc355886e948ce
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-21 11:53:50 -0700

    lambda func

--- CallingMethods6_TA/CallingMethods6_TA/Program.cs
+++ CallingMethods6_TA/CallingMethods6_TA/Program.cs
@@ -10,6 +10,47 @@ namespace CallingMethods6_TA
     {
         static void Main()
         {
+            //LAMBDA ASSIGNMENT step 338
+
+            Employee<int> employee4 = new Employee<int>() { FirstName = "Joe", LastName = "Pupil", id = 023 };
+            Employee<int> employee5 = new Employee<int>() { FirstName = "Joe", LastName = "Pupil", id = 024 };
+            Employee<int> employee6 = new Employee<int>() { FirstName = "Rick", LastName = "Pupil", id = 025 };
+            Employee<int> employee7 = new Employee<int>() { FirstName = "Fred", LastName = "Pupil", id = 026 };
+            Employee<int> employee8 = new Employee<int>() { FirstName = "George", LastName = "Pupil", id = 027 };
+            Employee<int> employee9 = new Employee<int>() { FirstName = "Ender", LastName = "Pupil", id = 028 };
+            Employee<int> employee10 = new Employee<int>() { FirstName = "Harry", LastName = "Pupil", id = 029 };
+            Employee<int> employee11= new Employee<int>() { FirstName = "Bilbo", LastName = "Pupil", id = 030 };
+            Employee<int> employee12 = new Employee<int>() { FirstName = "mack", LastName = "Pupil", id = 031 };
+            Employee<int> employee13 = new Employee<int>() { FirstName = "Daddy", LastName = "Pupil", id = 032 };
+            List<Employee<int>> Walmart = new List<Employee<int>>() { employee4,employee5,employee6,employee7,employee8,employee9,employee10,employee11,employee12,employee13};
+            Console.WriteLine("All employees in list:");
+            foreach (Employee<int> emp in Walmart)
+            {
+                Console.WriteLine("{0} {2} has an id of {1}", emp.FirstName, emp.id, emp.LastName);
+            }
+
+            List<Employee<int>> WalmartJoes = new List<Employee<int>>();
+            //adding joes to new list
+            //foreach (Employee<int> emp in Walmart)
+            //{
+            //    if (emp.FirstName == "Joe")
+            //    {
+            //        WalmartJoes.Add(emp);
+            //    }
+            //}
+            //same task with lambda func
+            WalmartJoes = Walmart.Where(x => x.FirstName == "Joe").ToList();
+
+            Console.WriteLine("All employees in list named Joe:");
+            foreach (Employee<int> emp in WalmartJoes)
+            {
+                Console.WriteLine("{0} has an id of {1}", emp.FirstName, emp.id);
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+
+
+
             //Add employees
             Employee<string> employee1 = new Employee<string>() { FirstName = "SamPle", LastName = "Student", id = 006 };
             Employee<string> employee2 = new Employee<string>() { FirstName = "SamPle", LastName = "Learner", id = 006 };
